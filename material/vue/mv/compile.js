be019c733f023e82240b0f3ebc0e31bd31dc688a
胥涛 <xutao@lezhixing.com.cn>
2019-04-26 16:28:07 +0800

    feat: vue algorithm lru

--- material/vue/mv/compile.js
+++ material/vue/mv/compile.js
@@ -1,3 +1,15 @@
+
+// complie
+// 将template中获取需要响应式的内容。然后通过订阅-发布；data的set触发发布.
+// 订阅-发布和get, set有什么关系？
+// 当在template中获取到需要响应式属性的时候，通过data[attr]的方式，触发get
+// 在get中将当前订阅-发布对象绑定到Dep.target上
+//（利用闭包: 在defineProperty的时候，为这个对象初始化一个订阅发布对象. 
+// 一个属性就对应着一个订阅发布对象，因为template中可能一个响应式属性多个地方用到。
+// 这就是为什么用订阅-发布模式）
+// 然后添加订阅。
+// 在data的属性发生了变化后，触发set。
+// 判断set触发后，判断val
 function compile(template, data, el) {
   // 将template中需要绑定的属性识别出来。
   var attrs = template.match(/(?<=\{\{)(.*?)(?=\}\})/g)
@@ -5,7 +17,6 @@ function compile(template, data, el) {
     let res = data[attr]
     if (Dep.target) {
       Dep.target.addSub(function (val) {
-        console.log('replace', attr, val)
         var html = template.replace('{{' + attr + '}}', val)
         el.innerHTML = html
       })
