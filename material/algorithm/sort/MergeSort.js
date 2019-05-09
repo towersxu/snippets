2f734431163cfa374db595597251c4ec1dc62b2b
胥涛 <xutao@lezhixing.com.cn>
2019-05-09 10:18:03 +0800

    feat: 归并排序注释

--- material/algorithm/sort/MergeSort.js
+++ material/algorithm/sort/MergeSort.js
@@ -9,7 +9,6 @@ function MergeSortC (arr, startIndex, endIndedx) {
   if (startIndex >= endIndedx) return;
 
   let middleIndex = Math.floor((startIndex + endIndedx) / 2);
-  console.log(middleIndex)
   MergeSortC(arr, startIndex, middleIndex);
   MergeSortC(arr, middleIndex + 1, endIndedx);
 
@@ -17,8 +16,7 @@ function MergeSortC (arr, startIndex, endIndedx) {
 }
 
 function merge (arr, startIndex, middleIndex, endIndedx) {
-  debugger
-  console.log(arr, startIndex, middleIndex, endIndedx)
+  let p = startIndex // 记录最开始的数组位置，用于最后的重新赋值
   let newArr = []
   let k = 0
   let j = middleIndex + 1
@@ -31,14 +29,14 @@ function merge (arr, startIndex, middleIndex, endIndedx) {
   }
   let start = startIndex
   let end = middleIndex
-  if (j < endIndedx) { // 表示后半部分没有排序完成
+  if (j <= endIndedx) { // 表示后半部分没有排序完成
     start = j
     end = endIndedx
   }
-  while (start <= end) {
+  while (start <= end) { // 没有排序完成的，都是有序的最大的内容
     newArr[k++] = arr[start++]
   }
-  for (let i = 0; i < endIndedx; i++) {
-    arr[startIndex + i] = newArr[i]
+  for (let i = 0; i < newArr.length; i++) {
+    arr[p + i] = newArr[i]
   }
 }
