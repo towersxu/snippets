7032d9f3fc6c2ffc211b731c7e8934f3464f6237
胥涛 <xutao@lezhixing.com.cn>
2019-06-03 09:57:54 +0800

    feat: 最长回文子字符串

--- material/algorithm/leetcode/0005_longestPalindrome.js
+++ material/algorithm/leetcode/0005_longestPalindrome.js
@@ -1,6 +1,38 @@
 // 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 var longestPalindrome = function (s) {
   // Manacher算法？？？
+  let t = '$#'
+  for (let i = 0; i < s.length; i++) {
+    t+= s[i] + '#'
+  }
+  /******************/
+  let rightIndex = 0;
+  let centerIndex = 0;
+  //求len中的最大
+  let answer = 0;
+  //answer最大时的中心
+  let index = 0;
+  let len = []
+  // $#c#s#a#s#s#a#s#d#f#a#s#a#f#d#s#d#
+  for (let i = 1; i < t.length; i++) {
+    if (rightIndex > i) {
+      len[i] = Math.min(rightIndex - i, len[2 * centerIndex - i]);
+    } else {
+      len[i] = 1;
+    }
+    while (t[i - len[i]] == t[i + len[i]]) {
+      len[i]++;
+    }
+    if (len[i] + i > rightIndex) {
+      rightIndex = len[i] + i;
+      centerIndex = i;
+    }
+    if (len[i] > answer) {
+      answer = len[i];
+      index = i;
+    }
+  }
+  return s.substr((index - answer) / 2, answer - 1);
 };
 // var longestPalindrome = function (s) {
 //   if (s == null || s.length < 1) return "";
@@ -25,4 +57,4 @@ var longestPalindrome = function (s) {
 //   return R - L - 1;
 // }
 
-console.log(longestPalindrome('csassasdfasfasd'))
+console.log(longestPalindrome('aaaaaav'))
