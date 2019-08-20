4a6106bdc34ad265f38dee69bf9b30b13a80525d
胥涛 <xutao@lezhixing.com.cn>
2019-08-20 11:02:21 +0800

    typescript demo

--- demo/typescript/demo1/Interface.ts
+++ demo/typescript/demo1/Interface.ts
@@ -34,3 +34,49 @@ let ro: ReadonlyArray<number> = a;
 
 // 最简单判断该用readonly还是const的方法是看要把它做为变量使用还是做为一个属性。 
 // 做为变量使用的话用const，若做为属性则使用readonly。
+
+interface SquareConfig {
+  color?: string;
+  width?: number;
+  [propName: string]: any;
+}
+
+function createSquare(config: SquareConfig) {
+  // ...
+}
+
+let mySquare = createSquare({ cx: "red", width: 100 });
+
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function(src: string, sub: string): boolean {
+  let result = src.search(sub);
+  return result > -1;
+}
+
+
+interface StringArray {
+  [index: number]: string;
+}
+
+let myArray: StringArray = ["Bob", "Fred"];
+
+let myStr: string = myArray[0];
+
+class Animal {
+  [index: string]: string;
+}
+class Dog extends Animal {
+  breed: string;
+}
+
+// 错误：使用数值型的字符串索引，有时会得到完全不同的Animal!
+// interface NotOkay {
+//   [x: number]: Animal;
+//   [x: string]: Dog;
+// }
+
+let a1: Animal = ['1', '2']
---
+++ material/js/awesome/blackhole.js
@@ -0,0 +1,5 @@
+for(i=0;
+i<2e3;
+x.fillRect(i?960+i*S(F=260*(t+9)/i+S(i*i)):0,
+i?500+.2*(2*i*C(F)+2e4/i):0,K=i++?S(i)*9:2e3,K)
+)x.fillStyle=R(99*i,2*i,i,i?1:.4)
