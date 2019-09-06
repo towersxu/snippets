4ff5797c4fd101f4400438d7c3593133930e4cb6
胥涛 <xutao@lezhixing.com.cn>
2019-09-06 09:51:49 +0800

    webaudio

--- material/webAudioAPI/demo2/audio.js
+++ material/webAudioAPI/demo2/audio.js
@@ -5,15 +5,46 @@ class CAudioNode {
     this.audioContext = new AudioContext()
     this.sourceNode = new CSourceNode(this.audioContext)
     this.jsNode = new CJsNode(this.audioContext)
+    this.analyNode = new CAnalyNode(this.audioContext)
+    this.gainNode = new CGainNode(this.audioContext)
+    this.pannerNode = new CPannerNode(this.audioContext)
   }
   start(arrayBuffer) {
     this.audioContext.decodeAudioData(arrayBuffer, (buffer) => {
-      this.sourceNode.addBuffer(buffer)
-      this.sourceNode.connect(this.jsNode).connect(this.audioContext.destination)
+      this.playBuffer(buffer)
     })
+  }
+  playBuffer (buffer) {
+    this.sourceNode.addBuffer(buffer)
+    this.sourceNode.connect(this.jsNode).connect(this.pannerNode).connect(this.analyNode).connect(this.gainNode).connect(this.audioContext.destination)
+    this.jsNode.listenAudioprocess(() => {
+      this.analyNode.getByteTimeDomainData();
+      this.analyNode.draw()
+    })
+  }
+  addBuffer (buffer) {
+    this.sourceNode.addBuffer(buffer)
   }
   stop() {
     this.sourceNode.stop()
+    this.jsNode.stop()
+  }
+  playRandomSound (isAdd) {
+    let channels = 1
+    let frameCount = this.audioContext.sampleRate * 2
+    let randomBuffer = this.audioContext.createBuffer(channels, frameCount, this.audioContext.sampleRate)
+    let arr = [-0.5, -0.2, 0.3, 0.4, 0.5, 0.6, -0.1, -0.2, -0.1, -0.2, -0.3, -0.5,  -0.3, -0.5, 0.1, 0.3, 0.2, 0.23, -0.5, -0.2, 0.4, 0.5, 0.6,0.1, 0.2, 0.23]
+    for (let channel = 0; channel < channels; channel++){
+      var nowBuffering = randomBuffer.getChannelData(channel)
+      for (let j = 0; j < frameCount; j++) {
+        nowBuffering[j] = arr[j % 18];
+      }
+    }
+    if (isAdd) {
+      this.addBuffer(randomBuffer)
+    } else {
+      this.playBuffer(randomBuffer)
+    }
   }
 }
 
@@ -27,12 +58,12 @@ class AudioNodeInterface {
     return this.node
   }
   connect (node) {
+    let prueNode = node
     if (node instanceof AudioNodeInterface) {
-      console.log(node)
-      node = node.getNode()
+      prueNode = node.getNode()
     }
-    this.node.connect(node)
-    return this
+    this.node.connect(prueNode)
+    return node
   }
 }
 
@@ -53,17 +84,21 @@ class CSourceNode extends AudioNodeInterface {
   }
 }
 
+// 
 class CJsNode extends AudioNodeInterface {
   constructor (audioContext) {
     super()
     this.bufferSize = 2048
-    this.numberOfInputChannels = 1
-    this.numberOfOutputChannels = 1
+    this.numberOfInputChannels = 2
+    this.numberOfOutputChannels = 2
+    this.isInjectAudioprocess = false
+    this.isInjectAudioprocessEverySample = false
+    this.injectAudioprocess = function () { }
+    this.injectAudioprocessEverySample = function () { }
     this.node = audioContext.createScriptProcessor(this.bufferSize, this.numberOfInputChannels, this.numberOfOutputChannels)
-    this.node.onaudioprocess = this.onaudioprocess
+    this.node.onaudioprocess = this.onaudioprocess.bind(this)
   }
   onaudioprocess (audioProcessingEvent) {
-    console.log(11)
     var inputBuffer = audioProcessingEvent.inputBuffer;
     var outputBuffer = audioProcessingEvent.outputBuffer;
     for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
@@ -71,8 +106,106 @@ class CJsNode extends AudioNodeInterface {
       var outputData = outputBuffer.getChannelData(channel);
       for (var sample = 0; sample < inputBuffer.length; sample++) {
         outputData[sample] = inputData[sample];
-        outputData[sample] += ((Math.random() * 2) - 1) * 0.4;
+        if (this.isInjectAudioprocessEverySample) {
+          outputData[sample] = this.injectAudioprocessEverySample(inputData[sample])
+        }
       }
     }
+    if (this.isInjectAudioprocess) {
+      this.injectAudioprocess(audioProcessingEvent)
+    }
+  }
+  listenAudioprocess (callback) {
+    this.isInjectAudioprocess = true
+    this.injectAudioprocess = callback
+  }
+  listenAudioprocessEverySample (callback) {
+    this.isInjectAudioprocessEverySample = true
+    this.injectAudioprocessEverySample = callback
+  }
+  stop () {
+    this.node.disconnect()
+  }
+}
+
+// 音频可视化
+class CAnalyNode extends AudioNodeInterface {
+  constructor(audioContext) {
+    super()
+    this.node = audioContext.createAnalyser()
+    this.fftSize = 2048;
+    this.node.fftSize = this.fftSize;
+    this.dataArray = new Uint8Array(this.fftSize);
+    this.ctx = document.getElementById('canvas').getContext("2d");
+  }
+  getDataArray () {
+    return this.dataArray
+  }
+  getByteTimeDomainData () {
+    this.node.getByteTimeDomainData(this.dataArray)
+    return this.dataArray
+  }
+  draw () {
+    requestAnimationFrame(() => {
+      this.ctx.clearRect(0, 0, 512, 216);
+      for (var i = 0; i < this.dataArray.length; i++) {
+        var value = this.dataArray[i] / 256;
+        var y = 216 - (216 * value) - 1;
+        this.ctx.fillStyle = '#f1ffff';
+        this.ctx.fillRect(i, y, 1, 1);
+      }
+    })
+  }
+}
+
+// 音量
+class CGainNode extends AudioNodeInterface {
+  constructor (audioContext) {
+    super()
+    this.node = audioContext.createGain()
+    this.setValue(0.5)
+    this.initListener()
+  }
+  setValue (val) {
+    this.node.gain.value = val
+  }
+  initListener () {
+    let vol = document.getElementById('volume')
+    let volValue = document.getElementById('volume-value')
+    vol.addEventListener('input', () => {
+      this.setValue(vol.value)
+      volValue.innerText = vol.value
+    })
+  }
+}
+
+// 声音立体化处理
+class CPannerNode extends AudioNodeInterface {
+  constructor (audioContext) {
+    super()
+    this.audioContext = audioContext
+    this.node = audioContext.createPanner()
+    this.node.panningModel = 'HRTF'
+    this.initListener()
+  }
+  setPositionX(val) {
+    this.node.positionX.setValueAtTime(val, this.audioContext.currentTime)
+  }
+  setPositionY(val) {
+    this.node.positionY.setValueAtTime(val, this.audioContext.currentTime)
+  }
+  initListener() {
+    let panner = document.getElementById('panner')
+    let pannerValue = document.getElementById('panner-value')
+    panner.addEventListener('input', () => {
+      this.setPositionX(panner.value)
+      pannerValue.innerText = panner.value
+    })
+    let pannerY = document.getElementById('pannerY')
+    let pannerYValue = document.getElementById('pannerY-value')
+    pannerY.addEventListener('input', () => {
+      this.setPositionY(pannerY.value)
+      pannerYValue.innerText = pannerY.value
+    })
   }
 }
--- material/webAudioAPI/demo2/demo2.js
+++ material/webAudioAPI/demo2/demo2.js
@@ -1,21 +1,8 @@
 import CAudioNode from './audio.js'
 
-// console.log(CAudioNode)
-
-let url = '../audio/music.mp3'
-
-
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-
-let audioContext
-window.requestAnimFrame = (function () {
-  return window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    function (callback, element) {
-      window.setTimeout(callback, 1000 / 60);
-    };
-})();
+// let url = '../audio/music.mp3'
+let url = '../audio/Tonight_I_Feel_Close_To_You.mp3'
+// let url = '../audio/piano.mp3'
 
 function loadMusic (url) {
   return new Promise((resolve, reject) => {
@@ -28,73 +15,6 @@ function loadMusic (url) {
   })
 }
 
-function addSourceNode (buffer, nextNode) {
-  var sourceNode = audioContext.createBufferSource();
-  sourceNode.buffer = buffer;
-  sourceNode.start();
-  if (nextNode) {
-    sourceNode.connect(nextNode)
-  } else {
-    sourceNode.connect(audioContext.destination);
-  }
-  return sourceNode;
-}
-var dataArray;
-var analyserNode;
-function addAnalyNode(nextNode) {
-  analyserNode = audioContext.createAnalyser();
-  analyserNode.fftSize = 2048;
-  var bufferLength = analyserNode.fftSize;
-  dataArray = new Uint8Array(bufferLength);
-  if (nextNode) {
-    analyserNode.connect(nextNode);
-  } else {
-    analyserNode.connect(audioContext.destination);
-  }
-  return analyserNode;
-}
-
-function addJavascriptNode(nextNode) {
-  var scriptNode = audioContext.createScriptProcessor(2048, 1, 1);
-  scriptNode.onaudioprocess = function (audioProcessingEvent) {
-    var inputBuffer = audioProcessingEvent.inputBuffer;
-    var outputBuffer = audioProcessingEvent.outputBuffer;
-    for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
-      var inputData = inputBuffer.getChannelData(channel);
-      var outputData = outputBuffer.getChannelData(channel);
-      for (var sample = 0; sample < inputBuffer.length; sample++) {
-        outputData[sample] = inputData[sample];
-        // outputData[sample] += ((Math.random() * 2) - 1) * 0.2;
-      }
-    }
-    analyserNode.getByteTimeDomainData(dataArray);
-    // console.log(1)
-    requestAnimFrame(drawTimeDomain);
-  }
-  if (nextNode) {
-    scriptNode.connect(nextNode);
-  } else {
-    scriptNode.connect(audioContext.destination);
-  }
-}
-var ctx = document.getElementById('canvas').getContext("2d");
-var barHeight
-var barWidth = 10
-var HEIGHT=216
-var x = 0;
-function drawTimeDomain() {
-  clearCanvas();
-  for (var i = 0; i < dataArray.length; i++) {
-    var value = dataArray[i] / 256;
-    var y = 216 - (216 * value) - 1;
-    ctx.fillStyle = '#0f01ff';
-    ctx.fillRect(i, y, 1, 1);
-    x += barWidth + 1;
-  }
-}
-function clearCanvas() {
-  ctx.clearRect(0, 0, 512, 216);
-}
 let cAudioNode
 function play () {
   loadMusic(url)
@@ -109,4 +29,13 @@ document.getElementById('play').addEventListener('click', function () {
 })
 document.getElementById('stop').addEventListener('click', function () {
   cAudioNode.stop();
+})
+let cAudioNode1
+document.getElementById('randomSound').addEventListener('click', function () {
+  cAudioNode1 = new CAudioNode();
+  cAudioNode1.playRandomSound()
+})
+
+document.getElementById('addSound').addEventListener('click', function () {
+  cAudioNode1.playRandomSound(true)
 })
