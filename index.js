62928f7ea86140fa9dc40300bf1722e45a8dc5ed
胥涛 <987427795@qq.com>
2019-11-12 09:14:05 +0800

    docs: add resume

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ function getMd (path) {
 }
 
 getMd(path.resolve(__dirname, './'))
+
+let dir = path.resolve(__dirname, '../source/_posts')
+delDir(dir)
+
 mdPath.map((p) => {
   let name = ''
   if (/(\w+?)\/([\u4e00-\u9fa5_a-zA-Z0-9]+)?\.md/.test(p)) {
@@ -25,14 +29,20 @@ mdPath.map((p) => {
   }
 })
 
-function getHashCode(str, caseSensitive) {
-  if (!caseSensitive) {
-    str = str.toLowerCase();
+function delDir(path, deep) {
+  let files = [];
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach((file, index) => {
+      let curPath = path + "/" + file;
+      if (fs.statSync(curPath).isDirectory()) {
+        delDir(curPath, true); //递归删除文件夹
+      } else {
+        fs.unlinkSync(curPath); //删除文件
+      }
+    });
+    if (deep) {
+      fs.rmdirSync(path);
+    }
   }
-  var hash = 1315423911, i, ch;
-  for (i = str.length - 1; i >= 0; i--) {
-    ch = str.charCodeAt(i);
-    hash ^= ((hash << 5) + ch + (hash >> 2));
-  }
-  return (hash & 0x7FFFFFFF);
 }
---
+++ interview/resume.md
@@ -0,0 +1,25 @@
+# 简历-前端工程师
+
+## 基本信息
+
+|<b>姓名</b>|胥涛|<b>性别</b>|男|
+|-------|-----------|-----------|----------|
+|<b>出生年月</b>|1991年2月|<b>工作年限</b>|5年|
+|<b>联系电话</b>|18301640806|<b>邮箱</b>|towersxu@gmail.com|
+|<b>毕业大学</b>|西南科技大学|<b>时间</b>|2010年9月-2014年7月|
+|<b>学历</b>|本科（学士学位）|<b>专业</b>|计算机科学与技术|
+|<b>github</b>|[https://github.com/towersxu](https://github.com/towersxu)|<b>博客</b>|[http://blog.hippor.com](http://blog.hippor.com)|
+
+## 技能描述
+
+1. 熟悉HTML、HTML5、Javascript、ES6、css、css3、jquery等前端基本技术。
+2. 熟练使用Vue，对vue相关的技术栈有丰富的开发实践经验。
+3. 对于next.js、typescript、preact、angularjs、等也有相关的开发经验。
+4. 熟悉webpack、babel、gulp等前端工程化相关技术。
+5. 对于jenkins、travis-ci等自动化相关技术有一定的实践经验。有编写在实际应用中的shell、nodejs自动部署脚本。
+6. 对nodejs、java、mysql、redis等有一定的开发经验。
+7. 了解webassembly、pwa、flutter等技术并开发过一些个人小项目。
+
+## 工作经历
+
+## 项目介绍
